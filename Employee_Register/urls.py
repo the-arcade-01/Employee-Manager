1bf74f4c6743ef128b8978d26fc41c5fd7995d37
the-arcade-01 <aashisharcade@gmail.com>
2020-12-28 05:39:04 +0530

    .

--- Employee_Register/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.employee_list,name='employee_list'),
-    path('form/',views.employee_form,name='employee_form'),
-    path('<int:id>/',views.employee_form,name='employee_update'),
-    path('delete/<int:id>',views.employee_delete,name='employee_delete'),
-]
